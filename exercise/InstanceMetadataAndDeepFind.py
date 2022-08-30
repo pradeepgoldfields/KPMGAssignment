6897d2f935527434ed0332468f6042ed06bd106c
Pradeep  Kumar <pradeepkumar@Pradeeps-MacBook-Pro.local>
2022-08-31 01:36:40 +0200

    rewriting function to work with lists

--- exercise/InstanceMetadataAndDeepFind.py
+++ exercise/InstanceMetadataAndDeepFind.py
@@ -88,7 +88,7 @@ class Exercise:
             obj = self.get_nested_value_with_list_support(obj[keys[0]], keys[1:])
         elif isinstance(obj, list):
             for item in obj:
-                obj = self.get_nested_value_with_list_support(item, keys)
+                obj = self.get_nested_value_with_list_support(obj[1:], keys)
         else:
             return obj
         return obj
