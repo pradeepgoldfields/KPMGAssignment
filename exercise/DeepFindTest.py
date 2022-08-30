47842a3ad1650a3d75a79a49a899b9f27b2ef910
Pradeep  Kumar <pradeepkumar@Pradeeps-MacBook-Pro.local>
2022-08-31 01:46:40 +0200

    rewriting function to work with lists

--- exercise/DeepFindTest.py
+++ exercise/DeepFindTest.py
@@ -11,6 +11,9 @@ class Test(unittest.TestCase):
     def test_1_get_nested_value(self):
         self.assertEqual(1, self.e.get_nested_value({"a": {"b": {"c": 1}}}, "a/b/c"))
 
+    def test_3_get_nested_value(self):
+        self.assertEqual([3, 1], self.e.get_nested_value({'a': 0, 'b': [1, {"c":[3, 1]}, 2]}, "b/c"))
+
 
 if __name__ == '__main__':
     # begin the unittest.main()
