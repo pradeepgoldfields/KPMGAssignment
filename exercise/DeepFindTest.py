8a9460606bdfb7cdf7a5908115593da6945393b1
Pradeep  Kumar <pradeepkumar@Pradeeps-MacBook-Pro.local>
2022-08-30 23:17:46 +0200

    adding some tests

--- exercise/DeepFindTest.py
+++ exercise/DeepFindTest.py
@@ -1,12 +1,12 @@
-import DeepFindTest
+import InstanceMetadataAndDeepFind as ExerciseClass
 import json
 import unittest
 
 class Test(unittest.TestCase):
-    e = Exercise()
+    e = ExerciseClass.Exercise()
     def test_0_get_nested_value(self):
         self.assertEqual(1, self.e.get_nested_value(json.dumps({"a": {"b": {"c": 1}}}), "a/b/c"))
-        self.assertEqual([1, 2], self.e.get_nested_value(json.dumps({'a': 0, 'b': [[1, 2]]}), "b/0/1"))
+        self.assertEqual([1, 2], self.e.get_nested_value(json.dumps({'a': 0, 'b': [[1, 2]]}), "b/0/1")))
 
 
 if __name__ == '__main__':
