cda6dffafa4d8cb78c82fda2f85de54370836292
Pradeep  Kumar <pradeepkumar@pradeeps-mbp.home>
2022-08-31 10:34:40 +0200

    exception handling

--- exercise/DeepFindTest.py
+++ exercise/DeepFindTest.py
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
         self.assertEqual([3, 1], self.e.get_nested_value({'a': 0, 'b': [1, {"c": [3, 1]}, 2]}, "b/c"))
 
     def test_4_get_nested_value(self):
-        self.assertEqual([ ], self.e.get_nested_value({'a': 0, 'b': [1, {"c": [3, 1]}, 2]}, "b/d"))
+        self.assertEqual(None, self.e.get_nested_value({'a': 0, 'b': [1, {"c": [3, 1]}, 2]}, "b/d"))
 
 
 if __name__ == '__main__':
