e34a63de2b7a986f48e37885ad662473bf83b37c
Pradeep  Kumar <pradeepkumar@pradeeps-mbp.home>
2022-08-31 10:21:02 +0200

    adding test cases

--- exercise/DeepFindTest.py
+++ exercise/DeepFindTest.py
@@ -12,7 +12,10 @@ class Test(unittest.TestCase):
         self.assertEqual(1, self.e.get_nested_value({"a": {"b": {"c": 1}}}, "a/b/c"))
 
     def test_3_get_nested_value(self):
-        self.assertEqual([3, 1], self.e.get_nested_value({'a': 0, 'b': [1, {"c":[3, 1]}, 2]}, "b/c"))
+        self.assertEqual([3, 1], self.e.get_nested_value({'a': 0, 'b': [1, {"c": [3, 1]}, 2]}, "b/c"))
+
+    def test_4_get_nested_value(self):
+        self.assertEqual([ ], self.e.get_nested_value({'a': 0, 'b': [1, {"c": [3, 1]}, 2]}, "b/d"))
 
 
 if __name__ == '__main__':
